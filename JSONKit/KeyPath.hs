119b3a0f0323d68f126112083dea95463c69193f
Daniel Choi <dhchoi@gmail.com>
2015-04-21 23:15:19 -0400

    notes

--- JSONKit/KeyPath.hs
+++ JSONKit/KeyPath.hs
@@ -17,6 +17,9 @@ import Control.Applicative
 
 
 
+-- These are output configs specific to TSV
+-- We should separate the evaluation functions from TSV-specific code
+
 
 data Config = Config {
     arrayDelim :: Text
